92deb7978be5098a8be19f9546fb849fa92022b3
Ronaldo Baltar <ronaldo.baltar@gmail.com>
2024-02-01 14:29:13 -0300

    Delete R directory

--- R/build2.R
+++
@@ -1,4 +0,0 @@
-# An optional custom script to run after Hugo builds your site.
-# You can delete it if you do not need it.
-#
-# Note to self: could try to create a twitter bot posting updates here.
